e4c415905a8f16996bef526cc2679fdf0ebd6d44
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-06-07 12:26:36 +0300

    Finish task

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,43 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      current = nodes_to_visit.pop()
+      if current['id'] == id:
+        return current
+
+      nodes_to_visit = current['children'] + nodes_to_visit
+    return None
+    
+  
+  def breadth_first_traversal(node):
+    result = []
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      # 1. Remove the first node from the `nodes_to_visit` list
+      node = nodes_to_visit.pop(0)
+      # 2. Add its value to the result list 
+      result.append(node['value'])
+      # 3. Add its children (if any) to the END of the `nodes_to_visit` list
+      nodes_to_visit = nodes_to_visit + node['children']
+
+    return result
+  
+  
+
+  def depth_first_traversal(node):
+    result = []
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      # 1. Remove the first node from the `nodes_to_visit` list
+      node = nodes_to_visit.pop(0)
+      # 2. Add its value to the result list
+      result.append(node['value'])
+      # 3. Add its children (if any) to the BEGINNING of the `nodes_to_visit` list
+      nodes_to_visit = node['children'] + nodes_to_visit
+
+    return result
